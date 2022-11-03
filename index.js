de7710a0649faabf83d456d2d7e3c9f48e1a525a
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-11-03 14:47:37 +0100

    made changes: 'setting up db'

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ const { Sequelize } = require('sequelize')
 dotenv.config()
 const app = express()
 
-const sequelize = new Sequelize('database', 'username', 'password', {
-  host: 'localhost',
-  dialect: 'postgres',
-  username: 'postgres',
-  password: '',
-  database: ''
-})
+const sequelize = new Sequelize(process.env.DB_CONNECTION_URL)
 
+try {
+  sequelize.authenticate();
+  console.log('Connection has been established successfully.');
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend Server is running @ http://localhost:"+process.env.PORT)
