97d803c40f7098b8eb7741cc659d2c0d8099eedc
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-11-03 14:27:23 +0100

    made changes: 'setting up db'

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const express = require('express')
+const dotenv = require("dotenv")
+const { Sequelize } = require('sequelize')
 
+dotenv.config()
 const app = express()
+
+const sequelize = new Sequelize('database', 'username', 'password', {
+  host: 'localhost',
+  dialect: 'postgres',
+  username: 'postgres',
+  password: '',
+  database: ''
+})
+
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend Server is running @ http://localhost:"+process.env.PORT)
