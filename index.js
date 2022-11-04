ac50b06c567292a8e60edb2e46a893b20fdf45fb
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-11-04 15:07:30 +0100

    made changes

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+const userRoute = require("./routes/user")
+const authRoute = require("./routes/auth")
+
+dotenv.config()
+const app = express()
+
+mongoose.connect(process.env.MONGO_URL)
+.then(() => console.log("DB Connection Successful"))
+.catch((err) => {
+  console.log(err)
+})
+
+app.use(express.json())
+app.use("/api/users", userRoute)
+app.use("/api/auth", authRoute)
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Backend Server is running @ http://localhost:"+process.env.PORT)
+})
+ 
