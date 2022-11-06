943bfd1b23ba6bca32ff3dad2bbf0e80c4b9d50c
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-11-06 11:52:14 +0100

    made changes:'created register router'

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require("mongoose")
 const dotenv = require("dotenv")
+const authRoute = require("./routes/auth")
 
 dotenv.config()
 const app = express()
@@ -12,6 +13,7 @@ mongoose.connect(process.env.MONGO_URL)
 })
 
 app.use(express.json())
+app.use("/api/auth", authRoute)
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend Server is running @ http://localhost:"+process.env.PORT)
