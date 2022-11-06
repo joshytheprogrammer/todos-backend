ef54e5174f6e0e59884f4fdc2889066fdcb29a56
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-11-06 11:53:13 +0100

    made changes:'created register router / auth functions'

---
+++ routes/auth.js
@@ -0,0 +1,20 @@
+const router = require('express').Router()
+const User = require("../models/User")
+const CryptoJs = require("crypto-js")
+
+router.post("/register", async (req, res) => {
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: CryptoJs.AES.encrypt(req.body.password, process.env.PASS_SEC).toString()
+  })
+
+  try {
+    const savedUser = await newUser.save()
+    res.status(200).json(savedUser)
+  } catch (err) {
+    res.status(500).json(err);
+  }
+})
+
+module.exports = router
