78069bff3b30f78f847a4499723e2403091bde2a
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-11-08 15:17:56 +0100

    made changes: ''

--- routes/auth.js
+++ routes/auth.js
@@ -36,7 +36,7 @@ router.post("/login", async (req, res) => {
       return
     }
 
-    const accessToken = jwt.sign({
+    const token = jwt.sign({
       id: user._id,
       isAdmin: user.isAdmin
     }, 
@@ -46,7 +46,7 @@ router.post("/login", async (req, res) => {
 
     const { password, ...others } = user._doc;
 
-    res.status(200).json({...others, accessToken})
+    res.status(200).json({...others, token})
   } catch(err) {
     res.status(500).json(err)
   }
