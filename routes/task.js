4ca6db528ad65e697310ff81958f942899ee31b4
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-12-10 09:15:24 +0100

    made changes

--- routes/task.js
+++ routes/task.js
@@ -1,7 +1,15 @@
 const router = require('express').Router()
 
 router.post("/create", async (req, res) => {
-  
+  console.log(req.body.task)
+
+  let d = new Date(req.body.task.date).valueOf()
+
+  console.log(d)
+
+  console.log(new Date(d).toString())
+
+  res.status(200).send("request succeeded")
 })
 
 module.exports = router
